4c03a0cf32b23d950d5d77157d48906b853d8273
David Villamil <davidfvillamilm@gmail.com>
2024-02-20 18:51:59 -0500

    empezar el proyecto

---
+++ Responsive_design/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <>
+            <main>
+                <section>Sección 1</section>
+                <section> Sección 2</section>
+                <section> Sección 3</section>
+                <section> Sección 4</section>
+            </main>
+        </>
+    )
+}
+
+export default Main
---
+++ Responsive_design/src/components/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from '../assets/img/logo.svg'
+import '../styles/Header.css'
+
+const Header = () => {
+    return(
+        <>
+            <header>
+                <img src={Logo} alt="" />
+                <div className="header--title-container">
+                    <h1>La Próxima revolución en el intercambio de cryptomonedas</h1>
+                    <p>Batatabit ayuda a navegar entre los diferentes precios y tendencias</p>
+                    <a href="" className="header--button">Conoce nuestros planes <span>i</span></a>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
